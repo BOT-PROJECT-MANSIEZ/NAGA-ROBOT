13c4de94344f6450627f11a93195b7f56a09fc66
BOT-PROJECT-MANSIEZ <81785721+BOT-PROJECT-MANSIEZ@users.noreply.github.com>
2021-04-14 10:59:37 +0700

    Update sample_config.py

--- NAGA-ROBOT/sample_config.py
+++ NAGA-ROBOT/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/NagaRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/NAGA-ROBOT/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
