37ff84d01351590eff30d5b8e2787898d9b5ff31
BOT-PROJECT-MANSIEZ <81785721+BOT-PROJECT-MANSIEZ@users.noreply.github.com>
2021-04-14 10:47:48 +0700

    Create ___mansiezz_tools.py

---
+++ NAGA-ROBOT/modules/___mansiezz_tools.py
@@ -0,0 +1,18 @@
+# © @bismillahselaluadaa
+
+__mod_name__ = "▫️NAGAROBOT SPECIAL TOOLS▫️"
+
+__help__ = """
+ ❍ /song [song name] :uploads the song in it's best quality available
+ 💡Ex: `/song Tentang Aku dan Dia`
+
+ ❍ /github [username] :Get information about a GitHub user.
+
+ ❍ /country [country name] :Gathering info about given country
+
+ ❍ Mansiezz [query] : Mansiezz answers the query
+  💡Ex: `Mansiez where is Indonesia?`
+
+ ❍ /encrypt: Encrypts The Given Text
+ ❍ /decrypt: Decrypts Previously Ecrypted Text
+"""
